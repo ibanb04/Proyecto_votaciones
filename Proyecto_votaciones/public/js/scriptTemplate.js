baee8e21b83d84ed6d309c099247a7692e8310ac
ivan bettin <ibanb040@gmail.com>
2018-05-04 20:05:13 +0000

    se agrego materialize

---
+++ Proyecto_votaciones/public/js/scriptTemplate.js
@@ -0,0 +1,23 @@
+
+	$(document).ready(function(){
+
+		$(".dropdown-trigger").dropdown();
+		$('select').material_select();
+		$('.modal').modal();
+		
+
+		var urlDelete = '#';
+			
+		$('a.btnDelete').click(function(e){
+			e.preventDefault();
+			urlDelete = $(this).attr('href');
+			$('#modal-confirmarEliminacion').modal('open');
+		});
+
+		$('a#btnEliminar').click(function(){
+			location.href = urlDelete;
+		});
+
+		
+
+	});
